637e2a36f02f4a48c726659e79c58044fdc8923e
Oliver Tupran <oliver.tupran@gmail.com>
2019-05-03 08:55:54 +0200

    `SparkApp` fails if no configuration is expected #9
    - added empty configuration if no configuration is actually found but also probably not expected
    - increased unit test coverage for `SparkApp`

---
+++ src/test/scala/org/tupol/spark/LocalSparkSession.scala
@@ -0,0 +1,60 @@
+package org.tupol.spark
+
+import org.apache.spark.sql.{ SQLContext, SparkSession }
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.scalatest.{ BeforeAndAfterEach, Suite }
+
+import scala.util.Try
+
+/**
+ * A fresh SparkSession, with the SparkContext and the SqlContext for each test in the Suite.
+ */
+trait LocalSparkSession extends BeforeAndAfterEach {
+  this: Suite =>
+
+  def master = "local[2]"
+  def appName = this.getClass.getSimpleName
+
+  @transient private var _spark: SparkSession = _
+
+  implicit lazy val spark: SparkSession = _spark
+
+  implicit lazy val sc: SparkContext = spark.sparkContext
+
+  implicit lazy val sqlContext: SQLContext = spark.sqlContext
+
+  def sparkConfig: Map[String, String] = Map("spark.driver.host" -> "localhost") //See https://issues.apache.org/jira/browse/SPARK-19394
+
+  def createSparkSession(conf: SparkConf): SparkSession =
+    SparkSession.builder.config(conf).getOrCreate()
+
+  override def beforeEach(): Unit = {
+    System.clearProperty("spark.driver.port")
+    System.clearProperty("spark.hostPort")
+
+    super.beforeEach()
+
+    val conf = new SparkConf()
+      .setMaster(master)
+      .setAppName(appName)
+
+    sparkConfig.foreach { case (k, v) => conf.setIfMissing(k, v) }
+
+    _spark = createSparkSession(conf)
+
+  }
+
+  override def afterEach(): Unit = {
+    try {
+      if (_spark != null) {
+        Try(_spark.close())
+        _spark = null
+      }
+    } finally {
+      super.afterEach()
+      System.clearProperty("spark.driver.port")
+      System.clearProperty("spark.hostPort")
+    }
+  }
+
+}
